b36d3303bf86fc078d85e61092a0e5f8c9668af7
jaehoon7368 <dbwogns7368@naver.com>
2023-01-24 19:26:35 +0900

    checkInOut Search 완료-재훈

--- src/main/java/com/sh/airbnb/hotel/controller/HotelSearchServlet.java
+++ src/main/java/com/sh/airbnb/hotel/controller/HotelSearchServlet.java
@@ -1,11 +1,16 @@
 package com.sh.airbnb.hotel.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sh.airbnb.hotel.model.dto.Hotel;
+import com.sh.airbnb.hotel.model.service.HotelService;
 
 /**
  * Servlet implementation class HotelSearchServlet
@@ -13,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/hotel/searchHotel")
 public class HotelSearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	private HotelService hotelService = new HotelService();
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -26,10 +31,14 @@ public class HotelSearchServlet extends HttpServlet {
 		System.out.println("checkIn = " + checkIn);
 		System.out.println("checkOut = " + checkOut);
 		
+		List<Hotel> hotelList = hotelService.hotelTotalView();
+		System.out.println("hotelList = " + hotelList);
+		
+		request.setAttribute("hotelList", hotelList);
 		request.setAttribute("searchLocation", searchLocation);
 		request.setAttribute("checkIn", checkIn);
 		request.setAttribute("checkOut", checkOut);
-		response.sendRedirect(request.getContextPath());
+		request.getRequestDispatcher("/WEB-INF/views/hotel/hotel.jsp").forward(request, response);
 	}
 
 }
